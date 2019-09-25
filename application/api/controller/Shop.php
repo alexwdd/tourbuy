8fc761f54afca980308848fd51089193cefe4011
alexwdd <alex_wdd@sina.com.cn>
2019-09-26 01:04:55 +0800

    0926

--- application/api/controller/Shop.php
+++ application/api/controller/Shop.php
@@ -42,24 +42,55 @@ class Shop extends Auth {
             if(!checkFormDate()){returnJson(0,'ERROR');}
 
             $cityID = input('post.cityID');
+            $cid = input('post.cid');
 
             $map['cate'] = 5;
-            $list = db('OptionItem')->field('id as cid,name')->where($map)->order('sort asc,id asc')->select();
-            foreach ($list as $key => $value) {
-                unset($map);
-                $map['cid'] = $value['cid'];
-                if($cityID>0){
-                    $map['cityID'] = $cityID;
-                }
-                $map['group'] = array('elt',$this->user['group']);
-                $child = db('Shop')->field('id,name,picname')->where($map)->order('id desc,py asc')->limit(6)->select();
-                foreach ($child as $k => $val) {
-                    $val['picname'] = getThumb($val["picname"],200,150);
-                    $child[$k]['picname'] = getRealUrl($val['picname']);
-                }
-                $list[$key]['child'] = $child;
+            $cate = db('OptionItem')->field('id as cid,name')->where($map)->order('sort asc,id asc')->select();
+
+            unset($map);
+            if($cid!=''){
+                $map['cid'] = $cid;
+            }            
+            if($cityID>0){
+                $map['cityID'] = $cityID;
             }
-            returnJson(1,'success',['shop'=>$list]);
+            $map['group'] = array('elt',$this->user['group']);
+            $list = db('Shop')->field('id,name,intr,cityID,banner,picname')->where($map)->order('id desc,py asc')->limit(6)->select();
+            foreach ($list as $k => $val) {
+                $val['picname'] = getThumb($val["picname"],200,200);
+                $list[$k]['picname'] = getRealUrl($val['picname']);
+
+                $val['banner'] = getThumb($val["banner"],760,300);
+                $list[$k]['banner'] = getRealUrl($val['banner']);
+
+                $list[$k]['cityName'] = db("City")->where('id',$val['cityID'])->value("name");
+            } 
+            returnJson(1,'success',['cate'=>$cate,'data'=>$list]);
+        }
+    }
+
+    public function shopAll(){
+        if(request()->isPost()){
+            if(!checkFormDate()){returnJson(0,'ERROR');}
+            $cid = input('post.cid');
+            $list = db("Shop")->field('py')->group('py')->order('py asc')->select();
+            foreach ($list as $key => $value) {
+                $map['py'] = $value['py'];
+                if($cid!='' && is_numeric($cid)){
+                    $map['cid'] = $cid;
+                }
+                $shop = db("Shop")->field('id,cityID,picname,name,intr')->where($map)->order('id desc')->select();
+                foreach ($shop as $k => $val) {
+                    $val['picname'] = getThumb($val['picname'],200,200);
+                    $shop[$k]['picname'] = getRealUrl($val['picname']);
+
+                    $shop[$k]['cityName'] = db("City")->where('id',$val['cityID'])->value("name");
+                }
+                $list[$key]['child'] = $shop;
+            }
+
+            $category = db("OptionItem")->field('id as cid,name')->where('cate',5)->order('sort asc,id asc')->select();
+            returnJson(1,'success',['category'=>$category,'shop'=>$list]);
         }
     }
 
